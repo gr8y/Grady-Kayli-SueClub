c83916ba1f7fda03ac3b25f919f737466762ba39
NHTI <nhti@conc.students.local>
2018-04-11 15:59:23 -0400

    Main Menu / Char Select
    The Main menu and character select testing has been combined with the main map
    
    -Grady and Kayli

---
+++ Area_Test/Assets/Test_Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+        
+    }
+
+   
+   
+
+}
---
+++ Area_Test/Assets/Test_Scripts/SelectionSCreen.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
+
+
+
+public class SelectionSCreen : MonoBehaviour {
+
+
+    public Sprite[] Gallery;
+    public Image DisplayImage;
+    public Button NextButton;
+    public Button PrevButton;
+    public int Index = 0;
+
+    public GameObject[] CharacterSelect;
+
+
+
+    public void NextImage()
+    {
+        // if (Index + 1 < Gallery.Length)
+        // {
+        //     Index++;
+        //  }
+        Index++;
+        if (Index >= Gallery.Length)
+        {
+            Index = 0;
+        }
+
+    }
+
+    public void PrevImage()
+    {
+        // if (Index - 1 < 0)
+        // {
+        //   Index--;
+        // }
+        Index--;
+        if (Index <= -1)
+        {
+            Index = (Gallery.Length - 1);
+        }
+
+    }
+
+    private void Update()
+    {
+        DisplayImage.sprite = Gallery[Index];
+    }
+
+}
