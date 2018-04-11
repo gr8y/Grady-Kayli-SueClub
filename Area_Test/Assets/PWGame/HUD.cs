3ee27710c995240e7c69e3cfb9461996a0e275d9
gr8y <37554278+gr8y@users.noreply.github.com>
2018-04-11 16:44:33 -0400

    DamageControl
    Enemy damage
    Partcile sytem fog
    Player health bars

---
+++ Area_Test/Assets/PWGame/HUD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : FrameworkHUD {
+
+    public int PlayerNumer = 0;
+    public int Shields = 0;
+    public int Health = 0;
+
+    public Text playerNumberField;
+    public Slider healthBar;
+
+    //public GameObject ActivePanel;
+    //public GameObject SpectatePanel;
+
+    public override void UpdateHUD()
+    {
+        if(playerNumberField)
+        {
+            playerNumberField.text = "Player " + PlayerNumer;
+        }
+        if(healthBar)
+        {
+            healthBar.value = Health;
+        }
+    }
+}
