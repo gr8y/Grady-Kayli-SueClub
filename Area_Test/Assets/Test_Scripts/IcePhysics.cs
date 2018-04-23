fe1218724debda24c9e0462a03e97ef916f67af8
d3dans <37554443+d3dans@users.noreply.github.com>
2018-04-23 14:51:08 -0400

    Update 4/23/18

---
+++ Area_Test/Assets/Test_Scripts/IcePhysics.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcePhysics : PWPlayerPawn
+{
+
+    // https://answers.unity.com/questions/240557/icyslippery-floor.html
+
+    //private Vector3 moveDir = Vector3.zero;
+    //private Vector3 lastMoveDir = Vector3.zero;
+    //private float slide = 1.75f;
+    //public bool icy = false;
+
+    /*public void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //moveDir = v * camera.forward + h * camera.right;
+        float inputMag = Mathf.Min(new Vector3(h, 0, v).sqrMagnitude, 1f);
+
+        if(inputMag > 0.225f)
+        {
+            lastMoveDir = moveDir;
+        }
+
+        if (icy)
+        {
+            moveDir = lastMoveDir * slide;
+        }
+        else
+        {
+            moveDir *= MoveSpeed;
+        }
+
+        //controller.Move(moveDir * Time.deltaTime);
+    }*/
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Ice")
+        {
+            GetComponent<Collider>().material.dynamicFriction = 0;
+        }
+    }
+
+}
